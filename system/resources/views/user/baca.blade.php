7fafaf2c3dfd27e4a01dd89f2f488950f31b1acb
Utin Chindy <utincindyselvira16@gmail.com>
2020-11-27 15:53:01 +0700

    Buat Form Komen

--- system/resources/views/user/baca.blade.php
+++ system/resources/views/user/baca.blade.php
@@ -76,45 +76,38 @@
                     </div>
                 </div>
                 <div class="row">
-                    <div class="col-lg-8 mx-auto text-center"><p class="large text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut eaque, laboriosam veritatis, quos non quis ad perspiciatis, totam corporis ea, alias ut unde.</p></div>
-                </div>
-            </div>
-        </section>
-        <!-- Contact-->
-        <section class="page-section" id="contact">
-            <div class="container">
-                <div class="text-center">
-                    <h2 class="section-heading text-uppercase">Contact Us</h2>
-                    <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
-                </div>
-                <form id="contactForm" name="sentMessage" novalidate="novalidate">
-                    <div class="row align-items-stretch mb-5">
-                        <div class="col-md-6">
-                            <div class="form-group">
-                                <input class="form-control" id="name" type="text" placeholder="Your Name *" required="required" data-validation-required-message="Please enter your name." />
-                                <p class="help-block text-danger"></p>
-                            </div>
-                            <div class="form-group">
-                                <input class="form-control" id="email" type="email" placeholder="Your Email *" required="required" data-validation-required-message="Please enter your email address." />
-                                <p class="help-block text-danger"></p>
-                            </div>
-                            <div class="form-group mb-md-0">
-                                <input class="form-control" id="phone" type="tel" placeholder="Your Phone *" required="required" data-validation-required-message="Please enter your phone number." />
-                                <p class="help-block text-danger"></p>
-                            </div>
+                    <div class="col-lg-12 mt-5">
+                    <h4 class="text-white ">Masukkan Komentar</h4>
+                        <p class="large text-muted ">
+                        <div class="card bg-dark ">
+                            <form action="{{url('komentar', $artikel->id)}}" method="post">
+                                @csrf
+                              <div class="form-row align-items-center">
+                                <div class="col-4">
+                                  <label for="inlineFormInput">Nama</label>
+                                  <input type="text" class="form-control mb-2" id="inlineFormInput" name="nama">
+                                </div>
+                                <div class="col-4">
+                                  <label for="inlineFormInputGroup">Alamat Email</label>
+                                  <div class="input-group mb-2">
+                                    <div class="input-group-prepend">
+                                      <div class="input-group-text">@</div>
+                                    </div>
+                                    <input type="text" class="form-control" id="inlineFormInputGroup" name="email">
+                                  </div>
+                                </div>
+                                <div class="col-4">
+                                  <button type="submit" class="btn btn-primary mt-5 btn-xl">Submit</button>
+                                </div>
+                              </div>
+                              <div class="form-group col-8 pl-0 pr-1">
+                                <label for="formGroupExampleInput">Isi Komentar</label>
+                                <textarea class="form-control" rows="5" name="isi"></textarea>
+                              </div>
+                            </form>
                         </div>
-                        <div class="col-md-6">
-                            <div class="form-group form-group-textarea mb-md-0">
-                                <textarea class="form-control" id="message" placeholder="Your Message *" required="required" data-validation-required-message="Please enter a message."></textarea>
-                                <p class="help-block text-danger"></p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="text-center">
-                        <div id="success"></div>
-                        <button class="btn btn-primary btn-xl text-uppercase" id="sendMessageButton" type="submit">Send Message</button>
-                    </div>
-                </form>
+                    </p></div>
+                </div>
             </div>
         </section>
         <!-- Footer-->
